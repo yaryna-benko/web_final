b9b6fe670db3e9cf35c96ff45ab9b739526fab7a
Yaryna Benko <yarynabenko@MacBook-Pro-Yaryna.local>
2021-05-24 17:07:12 +0300

    added comments and css

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
+  
+    has_many :comments, dependent: :destroy
+  
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 1 }
-end
+  end
+
+  

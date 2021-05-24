69b611101da68c29ae55fb36f246975fb7a2c28f
Yaryna Benko <yarynabenko@MacBook-Pro-Yaryna.local>
2021-05-24 21:11:37 +0300

    Added images

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   
     has_many :comments, dependent: :destroy
+    has_one_attached :image
   
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 1 }
